7847afab8301efe06627968695af38c484f9f00f
DevXML <50839056+DevXML@users.noreply.github.com>
2020-02-26 19:24:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,143 @@
+const Discord = require("discord.js"),
+   client = new Discord.Client({ disableEveryone: true }),
+   prefix = ";",
+   token = "";
+
+client.on("ready",async() =>{
+   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+   client.user.setActivity (`${client.guilds.size} guilds.`, {type:3})
+});
+
+client.on("message",async(message) => {
+   if(message.author.bot || message.content.indexOf(prefix) !== 0) return;
+
+
+   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+     const command = args.shift().toLowerCase();
+   switch(command) {
+       case "game":
+           return message.reply(`I am currently playing ${client.user.presence.game.name}!`);
+           break;
+   }
+});
+
+client.on("ready", () => {
+ console.log("ready to rumble!");
+});
+
+
+var bannedwords = "buster".split(",");
+
+client.on("message", msg => {
+ if (msg.guild === null) return;
+
+ for (i=0;i<bannedwords.length;i++) {
+   if (msg.content.toLowerCase().includes(bannedwords[i])) {
+     msg.delete();
+     msg.reply("Please don't swear!");
+     return;
+   }
+ }
+
+ if (msg.author.bot) return;
+ if (!msg.member.hasPermission("KICK_MEMBERS")) return;
+ if (!msg.content.toLowerCase().startsWith(prefix)) return;
+ msg.delete();
+ if (msg.content.toLowerCase().startsWith(prefix + "kick ")) {
+   var mem = msg.mentions.members.first();
+   mem.kick().then(() => {
+     msg.channel.send(mem.displayName + " has successfully been kicked by " + msg.author.username + "!");
+   }).catch(e => {
+     msg.channel.send("An error occured!");
+   });
+ }
+ if (!msg.member.hasPermission("BAN_MEMBERS")) return;
+
+ if (msg.content.toLowerCase().startsWith(prefix + "ban ")) {
+   var mem = msg.mentions.members.first();
+   var mc = msg.content.split(" ")[2];
+   mem.ban(mc).then(() => {
+     msg.channel.send(mem.displayName + " has successfully been banned by " + msg.author.username + " for " + mc + " days!");
+   }).catch(e => {
+     msg.channel.send("An error occured!");
+   });
+ }
+ if (!msg.member.hasPermission("MANAGE_ROLES")) return;
+
+ if (msg.content.toLowerCase().startsWith(prefix + "mute")) {
+   var mem = msg.mentions.members.first();
+   if (msg.guild.roles.find("name", "Muted")) {
+     mem.addRole(msg.guild.roles.find("name", "Muted")).then(() => {
+       msg.channel.send(mem.displayName + " has successfully been muted!");
+     }).catch(e => {
+       msg.channel.send("An error occured!");
+       console.log(e);
+     });
+
+   }
+ }
+ if (msg.content.toLowerCase().startsWith(prefix + "unmute")) {
+   var mem = msg.mentions.members.first();
+   if (msg.guild.roles.find("name", "Muted")) {
+     mem.removeRole(msg.guild.roles.find("name", "Muted")).then(() => {
+       msg.channel.send(mem.displayName + " has successfully been unmuted!");
+     }).catch(e => {
+       msg.channel.send("An error occured!");
+       console.log(e);
+     });
+
+   }
+ }
+ if (!msg.member.hasPermission("MANAGE_MESSAGES")) return;
+
+ if (msg.content.toLowerCase().startsWith(prefix + "purge")) {
+   var mc = msg.content.split(" ")[1];
+   msg.channel.bulkDelete(mc);
+ }
+
+client.on('guildMemberAdd', member => {
+ const channel = member.guild.channels.find(ch => ch.name === 'welcome-goodbye');
+ if (!channel) return;
+ channel.send(`Welcome to the server, ${member}`);
+});
+
+client.on('message', message => {
+ if (message.content === ';avatar') {
+   message.reply(message.author.avatarURL);
+ }
+});
+
+
+client.on('message', message => {
+ if (message.content === ';ping') {
+   message.channel.send('pong');
+ }
+});
+
+
+
+client.on('message', message => {
+ if (message.content === ';version') {
+   message.channel.send('Bloxtel is currently running Version 15.1.');
+ }
+});
+
+client.on('message', message => {
+ if (message.content === ';help') {
+   message.channel.send('My prefix is ;. Commands: ban, mute, unmute, kick, help, version, ping, invite, avatar, and guilds');
+ }
+});
+
+
+
+client.on('message', message => {
+ if (message.content === ';invite') {
+   message.channel.send('https://bit.ly/bloxtel');
+ }
+});
+
+
+
+});
+
+client.login('Njc3MjU3NDgwNzQ0NzMwNjI0.Xk1oZQ.oPbi4Z8GDRvcFntFoZilo6ITy3U');
